389ebd6d3017d41a1a573a8ca8545bfe9cd2796c
David Gregory <david.gregory@opencastsoftware.com>
2021-10-05 10:34:40 +0100

    CGBA-48 Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,14 @@ import sbt._
 
 object AppDependencies {
   val catsVersion       = "2.6.1"
-  val catsEffectVersion = "3.2.1"
+  val catsEffectVersion = "3.2.9"
   val log4catsVersion   = "2.1.1"
   val bootstrapVersion  = "5.14.0"
 
   val compile = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % bootstrapVersion,
     "uk.gov.hmrc"   %% "play-json-union-formatter" % "1.15.0-play-28",
-    "io.lemonlabs"  %% "scala-uri"                 % "3.5.0",
+    "io.lemonlabs"  %% "scala-uri"                 % "3.6.0",
     "org.typelevel" %% "cats-core"                 % catsVersion,
     "org.typelevel" %% "cats-effect"               % catsEffectVersion,
     "org.typelevel" %% "log4cats-slf4j"            % log4catsVersion,
