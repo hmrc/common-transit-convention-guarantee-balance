700f763fff161cc1945d4b53374435e2fe999aa7
David Gregory <david.gregory@opencastsoftware.com>
2021-11-04 13:40:28 +0000

    CGBA-86 Add rate limiting of user balance requests by GRN

---
+++ app/services/BalanceRequestLockService.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import cats.effect.IO
+import com.google.inject.ImplementedBy
+import config.AppConfig
+import models.values.GuaranteeReference
+import models.values.InternalId
+import runtime.IOFutures
+import uk.gov.hmrc.mongo.lock.MongoLockRepository
+
+import javax.inject.Inject
+
+@ImplementedBy(classOf[BalanceRequestLockServiceImpl])
+trait BalanceRequestLockService {
+  def isLockedOut(grn: GuaranteeReference, internalId: InternalId): IO[Boolean]
+}
+
+class BalanceRequestLockServiceImpl @Inject() (lockRepo: MongoLockRepository, appConfig: AppConfig)
+  extends BalanceRequestLockService
+  with IOFutures {
+
+  def isLockedOut(grn: GuaranteeReference, internalId: InternalId): IO[Boolean] = IO.runFuture {
+    implicit ec =>
+      lockRepo
+        .takeLock(
+          lockId = grn.value,
+          owner = internalId.value,
+          ttl = appConfig.balanceRequestLockoutTtl
+        )
+        .map(lockTaken => !lockTaken)
+  }
+}
