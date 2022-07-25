ac8d169c100dabe06013481144abe7f77b3b8787
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2022-07-25 10:57:14 +0100

    CTCP-843: Update MongoVersion based on bobby rule violation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val catsEffectVersion = "3.2.9"
   val log4catsVersion   = "2.1.1"
   val bootstrapVersion  = "5.19.0"
-  val hmrcMongoVersion  = "0.59.0"
+  val hmrcMongoVersion  = "0.68.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVersion,
